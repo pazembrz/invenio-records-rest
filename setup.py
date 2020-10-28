1f20c35449f4cfa59bcbbb86051c96446c1ca951
pazembrz <zembrzuski.pawel@gmail.com>
2020-10-28 15:09:10 +0100

    Make tests working

--- setup.py
+++ setup.py
@@ -17,17 +17,11 @@ readme = open('README.rst').read()
 history = open('CHANGES.rst').read()
 
 tests_require = [
-    'check-manifest>=0.25',
-    'coverage>=4.0',
     'Flask-Login>=0.3.2',
     'invenio-db[all]>=1.0.2',
     'invenio-indexer>=1.0.0',
-    'isort>=4.3.1',
-    'pydocstyle>=1.0.0',
-    'pytest-cov>=1.8.0',
-    'pytest-pep8>=1.0.6',
-    'pytest>=4.0.0,<5.0.0',
     'invenio-config>=1.0.2',
+    'pytest-invenio>=1.2.1',
 ]
 
 invenio_search_version = '1.2.3,<1.3.0'
@@ -53,7 +47,7 @@ extras_require = {
         'datacite>=1.0.1',
     ],
     'docs': [
-        'Sphinx>=1.6.7,<3',
+        'Sphinx>=3',
     ],
     'dublincore': [
         'dcxml>=0.1.0',
@@ -72,23 +66,21 @@ for name, reqs in extras_require.items():
     extras_require['all'].extend(reqs)
 
 setup_requires = [
-    'Babel>=1.3',
-    'pytest-runner>=2.6.2'
+    'Babel>=2.8',
 ]
 
 install_requires = [
     'arrow>=0.12.1',
     'attrs>=17.4.0',
     'bleach>=2.1.3',
-    'ftfy>=4.4.3,<5.0',
-    'Flask-BabelEx>=0.9.4',
+    'ftfy>=4.4.3',
     'invenio-base>=1.2.2',
-    'invenio-pidstore>=1.1.0',
-    'invenio-records>=1.3.0',
-    'invenio-rest>=1.1.3',
-    'invenio-indexer>=1.1.1',
+    'invenio-pidstore>=1.2.0',
+    'invenio-records>=1.0.0',
+    'invenio-rest==1.1.3',
+    'invenio-indexer>=1.1.0',
+    'invenio-i18n>=1.2.0',
     'python-dateutil>=2.4.2',
-    'six>=1.12',
 ]
 
 packages = find_packages()
@@ -146,10 +138,10 @@ setup(
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Development Status :: 5 - Production/Stable',
     ],
 )
