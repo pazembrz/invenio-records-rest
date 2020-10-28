08e0f3613c067c89de16342335998b0313706755
pazembrz <zembrzuski.pawel@gmail.com>
2020-10-28 15:09:10 +0100

    Make tests working

--- run-tests.sh
+++ run-tests.sh
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2015-2020 CERN.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-pydocstyle invenio_records_rest tests docs && \
-isort -rc -c -df && \
-check-manifest --ignore ".travis-*" && \
-sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test
+python -m check_manifest --ignore ".travis-*" && \
+python -m sphinx.cmd.build -qnN docs docs/_build/html && \
+python -m pytest && \
+python -m sphinx.cmd.build -qnN -b doctest docs docs/_build/doctest
+tests_exit_code=$?
+exit "$tests_exit_code"
